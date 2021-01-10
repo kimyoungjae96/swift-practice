616b9455d3a946293d4057b46b8485f39f5d0907
kimyoungjae <sskim5421@gmail.com>
2021-01-10 16:52:52 +0900

    add Tab Example

---
+++ 10/Tab/Tab/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Tab
+//
+//  Created by crerar on 2021/01/10.
+//  Copyright © 2021 youngjae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func btnMoveImageView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 1
+    }
+    @IBAction func btnMoveDatePickerView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 2
+    }
+}
+
