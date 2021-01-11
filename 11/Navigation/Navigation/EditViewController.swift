71c372569bb3e8a7292d14f6e69acd3da6ee5f5c
kimyoungjae <sskim5421@gmail.com>
2021-01-11 22:06:37 +0900

    add Navigation Example

---
+++ 11/Navigation/Navigation/EditViewController.swift
@@ -0,0 +1,61 @@
+//
+//  EditViewController.swift
+//  Navigation
+//
+//  Created by crerar on 2021/01/10.
+//  Copyright © 2021 youngjae. All rights reserved.
+//
+
+import UIKit
+
+protocol EditDelegate{
+    func didMessageEditDone(_ controller: EditViewController, message: String)
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool)
+}
+
+class EditViewController: UIViewController {
+
+    var textWayValue: String = ""
+    var textMessage: String = ""
+    var delegate : EditDelegate?
+    var isOn = false
+    
+    @IBOutlet var lblWay: UILabel!
+    @IBOutlet var txMessage: UITextField!
+    @IBOutlet var swIsOn: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        lblWay.text = textWayValue
+        txMessage.text = textMessage
+        swIsOn.isOn = isOn
+    }
+    
+    @IBAction func swImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            isOn = true
+        } else {
+            isOn = false
+        }
+    }
+    
+    @IBAction func btnDone(_ sender: UIButton) {
+        if delegate != nil {
+            delegate?.didMessageEditDone(self, message: txMessage.text!)
+            delegate?.didImageOnOffDone(self, isOn: isOn)
+        }
+        _ = navigationController?.popViewController(animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 11/Navigation/Navigation/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by crerar on 2021/01/10.
+//  Copyright © 2021 youngjae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, EditDelegate {
+    
+    let imgOn = UIImage(named: "lamp_on.png")
+    let imgOff = UIImage(named: "lamp_off.png")
+    
+    var isOn = true
+
+    @IBOutlet var txMessage: UITextField!
+    @IBOutlet var imgView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        imgView.image = imgOn
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let editViewController = segue.destination as! EditViewController
+        if segue.identifier == "editButton" {
+            editViewController.textWayValue = "segue : use button"
+        } else if segue.identifier == "editBarButton" {
+            editViewController.textWayValue = "segue : use Bar button"
+        }
+        editViewController.isOn = isOn
+        editViewController.textMessage = txMessage.text!
+        editViewController.delegate = self
+    }
+
+    func didMessageEditDone(_ controller: EditViewController, message: String) {
+        txMessage.text = message
+    }
+    
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool){
+        if isOn {
+            imgView.image = imgOn
+            self.isOn = true
+        } else {
+            imgView.image = imgOff
+            self.isOn = false
+        }
+    }
+}
+
