8566d05e6891031810cc2ce71e362890026129fc
kimyoungjae <sskim5421@gmail.com>
2021-11-25 00:03:11 +0900

    composing-complex-interfaces done

--- Landmarks/Landmarks/Model/ModelData.swift
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -10,6 +10,17 @@ import Combine
 
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
+    
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
